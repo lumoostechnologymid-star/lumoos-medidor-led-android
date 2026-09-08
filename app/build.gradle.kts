589f630bd684481f23f878a32fe411ab109796e3
lumoostechnologymid-star <lumoostechnologymid@gmail.com>
2026-09-08 10:32:35 -0600

    Usar Android SDK 36 para compilación estable

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,12 +8,12 @@ plugins {
 
 android {
     namespace = "com.lumoos.medidorled"
-    compileSdk = 37
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.lumoos.medidorled"
         minSdk = 23
-        targetSdk = 37
+        targetSdk = 36
         versionCode = 1
         versionName = "0.1.0"
     }
